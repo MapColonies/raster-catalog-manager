1f31abd4dc8e0eb06396678a7ee3695e42bec9dc
Shahar s <cl_shaharshavit@rnd-hub.com>
2021-10-07 11:50:55 +0300

    fix: change resolution db type to varchar

--- src/DAL/migration/v1.2.0.sql
+++ src/DAL/migration/v1.2.0.sql
@@ -1,4 +1,4 @@
 ALTER TABLE public.records
-    ADD COLUMN max_resolution_meter numeric(6, 2),
+    ADD COLUMN max_resolution_meter VARCHAR(10),
     ADD COLUMN raw_product_data jsonb,
     ADD COLUMN product_bbox VARCHAR(255);
