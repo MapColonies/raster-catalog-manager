ec28d8e17b18116adbaf5336199f8e98ce3b527a
Shahar s <cl-shaharshavit-rnd@users.noreply.github.com>
2021-11-09 08:33:17 +0200

    chore: used logger mock for integration tests

--- tests/testContainerConfig.ts
+++ tests/testContainerConfig.ts
@@ -1,14 +1,9 @@
-import { readFileSync } from 'fs';
 import { container } from 'tsyringe';
 import config from 'config';
-import { MCLogger, IServiceConfig } from '@map-colonies/mc-logger';
 import { Services } from '../src/common/constants';
+import { logger } from './mocks/Logger';
 
 function registerTestValues(): void {
-  const packageContent = readFileSync('./package.json', 'utf8');
-  const service = JSON.parse(packageContent) as IServiceConfig;
-  const logger = new MCLogger({ log2console: true, level: 'error' }, service);
-
   container.register(Services.CONFIG, { useValue: config });
   container.register(Services.LOGGER, { useValue: logger });
 }
