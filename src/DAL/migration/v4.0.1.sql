2850438afb45a035cd0731976bee33b9615c1e94
nitzanmorr <nitzan.morr@gmail.com>
2023-10-26 14:28:03 +0300

    feat: added constraint to db schema and a new matching migration

--- src/DAL/migration/v4.0.1.sql
+++ src/DAL/migration/v4.0.1.sql
@@ -1,3 +1,4 @@
 SET search_path TO "RasterCatalogManager", public; -- CHANGE SCHEMA NAME TO MATCH ENVIRONMENT
 ALTER TABLE "records" 
     ADD CONSTRAINT proper_product_version CHECK (product_version ~* '^\d+\.\d{1,2}$');
+    
