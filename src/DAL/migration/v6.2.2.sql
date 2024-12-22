158e530473120e318b088480b985da32aa61516a
Nitzan Morr <nitzan.morr@gmail.com>
2024-12-22 10:37:23 +0200

    fix: update migration to readd horizontal accuracy constraint

--- src/DAL/migration/v6.2.2.sql
+++
@@ -1,5 +0,0 @@
-  SET search_path TO "RasterCatalogManager", public; -- CHANGE SCHEMA NAME TO MATCH ENVIRONMENT
-
-ALTER TABLE "records" 
-  ALTER COLUMN min_horizontal_accuracy_ce_90 TYPE numeric USING (min_horizontal_accuracy_ce_90::numeric),
-  ALTER COLUMN max_horizontal_accuracy_ce_90 TYPE numeric USING (max_horizontal_accuracy_ce_90::numeric)
