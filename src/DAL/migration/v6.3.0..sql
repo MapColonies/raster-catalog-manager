8c508c472bad9c47ca139a9956034d40eb0c8f28
almog8k <almog8k@gmail.com>
2025-01-29 14:54:16 +0200

    feat: update fullSchema.sql not null columns and create new migration file

---
+++ src/DAL/migration/v6.3.0..sql
@@ -0,0 +1,9 @@
+  SET search_path TO "RasterCatalogManager", public; -- CHANGE SCHEMA NAME TO MATCH ENVIRONMENT
+
+
+ALTER TABLE records 
+    ALTER COLUMN creation_date_utc SET NOT NULL,
+    ALTER COLUMN imaging_time_begin_utc SET NOT NULL,
+    ALTER COLUMN imaging_time_end_utc SET NOT NULL,
+    ALTER COLUMN srs SET NOT NULL,
+    ALTER COLUMN srs_name SET NOT NULL;
