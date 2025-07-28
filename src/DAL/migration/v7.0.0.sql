7851e560ee35de89f9abf554e4148bf3fe9ee2d8
Shlomi k <65117898+CL-SHLOMIKONCHA@users.noreply.github.com>
2025-07-28 18:36:22 +0300

    fix: product status published for existing records (#81)

--- src/DAL/migration/v7.0.0.sql
+++ src/DAL/migration/v7.0.0.sql
@@ -3,3 +3,7 @@ SET search_path TO "RasterCatalogManager", public; -- CHANGE SCHEMA NAME TO MATC
 CREATE TYPE product_status AS ENUM ('PUBLISHED', 'UNPUBLISHED'); 
 ALTER TABLE "records" 
   ADD COLUMN product_status product_status NOT NULL DEFAULT 'UNPUBLISHED';
+
+-- Update all existing records to 'PUBLISHED'
+UPDATE "records" 
+SET product_status = 'PUBLISHED';
